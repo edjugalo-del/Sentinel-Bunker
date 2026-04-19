6fa8549f02958dda309a5175eaec9ccfbdd4671b
edjugalo-del <edjugalo@gmail.com>
2026-04-19 20:48:06 -0300

    Update SENTINEL_QUANT.py

--- SENTINEL_QUANT.py
+++ SENTINEL_QUANT.py
@@ -61,8 +61,10 @@ df_radar = pd.DataFrame(list(f_init.items()), columns=['ACTIVO', 'SCORE IA'])
 df_radar['PRECIO ACT'] = df_radar['ACTIVO'].apply(get_price)
 df_radar['ATTENTION'] = df_radar['ACTIVO'].apply(logica_atencion_tft)
 hum_data = df_radar['ACTIVO'].apply(obtener_humor_sentinel)
-df_radar['SCORE HUMOR'] = [x for x in hum_data]
-df_radar['HUMOR'] = [x for x in hum_data]
+hum_data = df_radar['ACTIVO'].apply(obtener_humor_sentinel)
+df_radar['SCORE HUMOR'] = [float(x[0]) for x in hum_data]
+df_radar['HUMOR'] = [str(x[1]) for x in hum_data]
+
 df_radar['CERTEZA'] = df_radar.apply(lambda x: inferencia_bayesiana(x['SCORE IA'], x['SCORE HUMOR'][0], x['ATTENTION']), axis=1)
 df_radar['KELLY %'] = df_radar['SCORE IA'].apply(lambda x: max(0, round((x * 2.5 - 1) / 1.5 / 4, 4)))
 df_radar['SUGERENCIA $'] = df_radar['KELLY %'] * st.session_state.liq
